788e4e06510ccee95f9690c58f8fc2c9215e00d2
ahmetcakmak <ahmetcakmak606@gmail.com>
2025-01-26 13:25:16 +0100

    push

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
 
 export default defineConfig({
   integrations: [
     tailwind(),
   ],
   output: 'static',
-  adapter: node({
-    mode: 'standalone'
-  }),
   i18n: {
     defaultLocale: 'nl',
     locales: ['nl', 'en', 'tr'],
